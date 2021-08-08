a2e76d17c5e81f202314f4a0b86c090a6d6e020b
JW406 <jwvic.2357@gmail.com>
2021-08-08 04:33:12 -0400

    Model schemas change; "Phase" now goes under "Iteration" instead of "Projects"

--- src/main/java/com/revature/registry/service/ProjectService.java
+++ src/main/java/com/revature/registry/service/ProjectService.java
@@ -8,40 +8,35 @@ import lombok.extern.slf4j.Slf4j;
 import com.revature.registry.model.Project;
 import com.revature.registry.repository.ProjectRepository;
 
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 public class ProjectService {
     private static Logger log = Logger.getLogger(ProjectService.class);
-    
-    
+
     private ProjectRepository pRepo;
-    
+
     public ProjectService() {
-        
+
     }
-    
+
     @Autowired
     public ProjectService(ProjectRepository pRepo) {
         this.pRepo = pRepo;
     }
-    
 
     public List<Project> getAllProjects() {
- //       log.setLevel(Level.DEBUG); if Logger is not working, use setLevel
-        log.debug("Fetching all products: " + pRepo.findAll());
+        //       log.setLevel(Level.DEBUG); if Logger is not working, use setLevel
+        // log.debug("Fetching all products: " + pRepo.findAll());
         return pRepo.findAll();
     }
 
     public Project getProjectById(int id) {
         Optional<Project> project = pRepo.findById(id);
         if (project.isPresent()) {
-            log.debug("Fetching Project with id of: " + project.get());
+            // log.debug("Fetching Project with id of: " + project.get());
             return project.get();
         }
         log.error("Unable to GET. Project with id " + id + " not found.");
@@ -51,7 +46,7 @@ public class ProjectService {
     public Project createProject(Project project) {
         Project savedProject = pRepo.save(project);
         log.debug("Project created with the following properties: " + savedProject.toString());
-       
+
         return savedProject;
     }
 
@@ -75,7 +70,7 @@ public class ProjectService {
             log.debug("Project Deleted with id: " + id);
             return true;
         }
-        log.error("Unable to DELETE. Project with id " + id+ " not found");
+        log.error("Unable to DELETE. Project with id " + id + " not found");
         return false;
     }
 
